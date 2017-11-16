686c109720ed251ab60dee6a3ebc880321dc8ef0
SilviaFogli <silviafogli93@gmail.com>
2017-11-16 10:14:34 +0100

    Prova

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML>
+<HEAD>
+      <TITLE>Pagina di prova in PHP</TITLE>
+</HEAD>
+<BODY>
+      <?php
+      print("Buongiorno a tutti!<br>\n");
+      echo(" Questo e' un corso bellissimo!");
+      ?>
+</BODY>
+</HTML>
